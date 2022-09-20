d1be228a1e7e32c2054337c89c20480168c8fc9e
jana-daouk <jana.daouk@student.allenisd.org>
2022-09-19 20:37:59 -0500

    two

--- main.swift
+++ main.swift
@@ -58,7 +58,6 @@ repeat {
 
 // removes the empty last one which then triggers the sorting function
 unsorted.removeLast()
-print(unsorted)
 // var sortText = unsorted.components(separatedBy: "\n")
 sort(array: unsorted)
 
